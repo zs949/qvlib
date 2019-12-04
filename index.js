86e15512d2a18eed1002766955bbc3e1942f88a1
zs <2388211949@qq.com>
2019-12-04 13:35:21 +0800

    数据交互lib文件

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var binary = require('node-pre-gyp');
+var path = require('path');
+var bindingPath = binary.find(path.resolve(path.join(__dirname, './package.json')));
+var binding = require(bindingPath);
+module.exports = exports = binding;
+/**
+  * The entry point.
+  *
+  * @module Sequelize
+  */
+// module.exports = require('./lib/binding/qvconci.node');
---
+++ test/t.js
@@ -0,0 +1,5 @@
+var qvlib = require('../index');
+
+console.log(qvlib.qv(1006,"131313131321313s")); // 'world'
+
+ 
